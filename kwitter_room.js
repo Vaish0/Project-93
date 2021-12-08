c97c0cadd8206d2daa779f3a395d20b087950a69
Vaish0 <89194528+Vaish0@users.noreply.github.com>
2021-12-08 06:59:23 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,8 @@
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+          Room_names = childKey;
+   
+         //End code
+         });});}
+   getData();
